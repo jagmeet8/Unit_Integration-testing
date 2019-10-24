cd9d720f60ddf9944589ddd2f6395894c5abdb66
Jagmeet Bali <jagmeetbali8@gmail.com>
2019-10-24 14:06:41 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+export const add = (x,y) => x + y ;
+
+
+export const total = (shipping, subtotal) => {
+return '$' + add(shipping, subtotal);
+};
---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import {add} from './App';
+
+test('add', () =>{
+    expect(add(1,2)).toBe(3);
+});
+
+
+
